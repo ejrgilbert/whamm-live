4a230e028621ca62aaea8b74679203a0e2d69fc8
Supreme Paudel <supremepaudel49@gmail.com>
2025-07-02 16:35:44 -0400

    Bug fix: Add tests for imports

--- src/model/fsm.ts
+++ src/model/fsm.ts
@@ -85,7 +85,7 @@ export class FSM{
 
         if (inject_type == 'module'){
             instance.stack.push(FSMHelper.wrap_stack_value(instance, inject_type));
-            FSMHelper.consume_until(['('], instance);
+            FSMHelper.consume_until_parenthesis(instance);
             instance.current_state = State.main_state;
         }
         else throw new Error("FSM parse Error: Expected 'module'!");
