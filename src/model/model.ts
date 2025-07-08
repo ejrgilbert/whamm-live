6c37c40ae80327cbd569acde889ae1521a5606b6
Supreme Paudel <supremepaudel49@gmail.com>
2025-07-08 11:06:59 -0400

    whammline-to-wasm-mapping: Modify the APIModel class to setup necessary data like fsm mappings on webview creation

--- src/model/model.ts
+++ src/model/model.ts
@@ -1,9 +1,70 @@
-// Class to store API responses [ MVC pattern's model ] 
+import { ExtensionContext } from "../extensionContext";
+import { WhammWebviewPanel } from "../user_interface/webviewPanel";
+import { FSM } from "../model/fsm";
 import { WhammResponse } from "./types";
+import * as vscode from 'vscode';
 
-export class Model{
-    static response: WhammResponse; 
-    // Will always be the latest API response with no error or 'null'
-    static no_error_response: WhammResponse | null; 
+// Class to store API responses [ MVC pattern's model ] 
+export class APIModel{
     static whamm_file_changing: boolean = false;
+
+    // will be null if wizard option chosen
+    valid_wat_content!: string | null;
+    valid_wasm_content!: Uint8Array | undefined;
+
+    response!: WhammResponse; 
+    webview: WhammWebviewPanel;
+    // Will always be the latest API response with no error or 'null'
+    no_error_response: WhammResponse | null; 
+    fsm_mappings: FSM | undefined;
+
+    constructor(webview: WhammWebviewPanel){
+        this.webview = webview;
+        this.no_error_response = null;
+    }
+
+    async loadWatAndWasm(): Promise<boolean>{
+        if (this.webview.fileName){
+            try{
+                let file_content = await vscode.workspace.fs.readFile(vscode.Uri.file(this.webview.fileName));
+                if (this.webview.is_wasm){
+                    [this.valid_wat_content, this.valid_wasm_content] = ExtensionContext.api.wasm2watandwasm(file_content);
+                } else
+                    [this.valid_wat_content, this.valid_wasm_content]= ExtensionContext.api.wat2watandwasm(
+                        new TextDecoder('utf-8').decode(file_content));
+            } catch(error){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // setup initial mappings and other necessary stuff
+    setup(): [boolean, string]{
+        // loadWatAndWasm should have been called to load the content
+        if (this.webview.fileName && this.valid_wasm_content && this.valid_wat_content){
+            try{
+                // setup in rust side
+                ExtensionContext.api.setup(this.webview.fileName, this.valid_wasm_content, {asMonitorModule: false});
+
+                // use the fsm to have the mappings ready
+                this.fsm_mappings = new FSM(this.valid_wat_content)
+
+                // create the mappings
+                this.update();
+
+                return [true, "success"];
+            } catch(error){
+                // do nothing since webviewPanel will handle all the related stuff with disposing
+            }
+        };
+        return [false, "Whamm setup failed"];
+    }
+
+    // Call the whamm API to update the model
+    update(): boolean{
+        // TODO
+        return true;
+
+    }
 }
