6c35e7be1b9339afc6d8930c411394f69a833696
Supreme Paudel <supremepaudel49@gmail.com>
2025-07-10 14:32:16 -0400

    injected-wat: Implement the structure for the `create_whamm_live_injection_instances` static method

--- src/model/utils/model_helper.ts
+++ src/model/utils/model_helper.ts
@@ -1,5 +1,6 @@
 import { Types } from "../../whammServer";
-import { WhammLiveInjection } from "../types";
+import { FSM } from "../fsm";
+import { InjectType, span, WatLineRange, WhammLiveInjection } from "../types";
 
 export class ModelHelper{
 
@@ -11,8 +12,8 @@ export class ModelHelper{
         let injected_wat = new Array(lines.length + number_of_lines_injected);
 
         for (let whamm_injection of whamm_injections){
-            for (let i =whamm_injection.range.l1; i <= whamm_injection.range.l2; i++){
-                injected_wat[i-1] = whamm_injection.code[i - whamm_injection.range.l1];
+            for (let i =whamm_injection.wat_range.l1; i <= whamm_injection.wat_range.l2; i++){
+                injected_wat[i-1] = whamm_injection.code[i - whamm_injection.wat_range.l1];
             }
         }
         
@@ -42,6 +43,8 @@ export class ModelHelper{
         return mapping;
     }
 
+    // Build a jagged array [ matrix ] based on the input provided
+    // every new line becomes a new row and every character in the line gets its own cell
     static create_jagged_array(string_contents: string): null[][]{
         let string_contents_array = string_contents.split('\n');
         let jagged_array: null[][]= new Array(string_contents_array.length);
@@ -50,5 +53,117 @@ export class ModelHelper{
             jagged_array[i] = new_array;
         }
         return jagged_array;
+    }
+
+    // Use the injection mappings created from `ModelHelper.create_whamm_data_type_to_whamm_injection_mapping`
+    // to create `WhammLiveInjection` instances. These instances will store their new wat locations
+    // This method returns a tuple with the first element being instances that are to be injected in the new wat
+    // and the other element being a list of instances that are to **not** be injected and instead be used as dangling references like `funcProbes`, `OpBodyProbes`, `Locals`
+    static create_whamm_live_injection_instances(fsm: FSM, whamm_live_mappings: Map<string, Types.WhammInjection[]>): [WhammLiveInjection[], WhammLiveInjection[]]{
+        // all the other injections except `funcProbes`, `OpBodyProbes`, `Locals` should update the number_of_lines_injected since they are literally injecting new wat content
+        var number_of_lines_injected = 0;
+        var whamm_live_injections_to_inject : WhammLiveInjection[]= [];
+        var whamm_live_injections_to_not_inject : WhammLiveInjection[]= [];
+
+        // follow the section orderings
+        for (let inject_type of
+            [Types.WhammDataType.typeType, Types.WhammDataType.importType, Types.WhammDataType.tableType, Types.WhammDataType.memoryType, Types.WhammDataType.globalType, Types.WhammDataType.exportType,
+            Types.WhammDataType.elementType, Types.WhammDataType.opProbeType, Types.WhammDataType.localType, Types.WhammDataType.funcProbeType, Types.WhammDataType.functionType, Types.WhammDataType.activeDataType, Types.WhammDataType.passiveDataType
+            ]){
+                // get the mapping
+                let injections =  whamm_live_mappings.get(Types.WhammDataType[inject_type]);
+                if (injections){
+                    for (let injection of injections){
+                        // for each of the the injection, create the appropriate injected content instance
+                        // and figure where they should be injected
+                        switch (inject_type){
+
+                            case Types.WhammDataType.importType:
+                                {
+                                    let import_injection = injection.importData;
+                                    if (import_injection === undefined) throw new Error("API response error: Import injectio is expected to be defined");
+
+                                    // use FSM to find where this injection should be injected
+                                    let start_wat_range = fsm.section_to_line_mapping.get(InjectType.Import);
+                                    if (start_wat_range === undefined) throw new Error("FSM error: Local fsm mapping not present");
+
+                                    // calculate the new wat line
+                                    let wat_line = start_wat_range + number_of_lines_injected;
+                                    let wat_range = {l1: wat_line,l2: wat_line} as WatLineRange;
+                                    let whamm_span = ModelHelper.get_whamm_span(import_injection.cause);
+
+                                    number_of_lines_injected++;
+                                    whamm_live_injections_to_inject.push(
+                                        {
+                                            type: Types.WhammInjectType.importInject,
+                                            code: [`(import "${import_injection.module}" "${import_injection.name}" (${import_injection.typeRef}))`],
+                                            wat_range: wat_range,
+                                            whamm_span: whamm_span
+                                        } as WhammLiveInjection
+                                    );
+                                }
+                                break;
+
+                            case Types.WhammDataType.exportType:
+                                break;
+
+                            case Types.WhammDataType.typeType:
+                                break;
+                            
+                            case Types.WhammDataType.memoryType:
+                                break;
+
+                            case Types.WhammDataType.activeDataType:
+                                break;
+
+                            case Types.WhammDataType.passiveDataType:
+                                break;
+
+                            case Types.WhammDataType.globalType:
+                                break;
+
+                            case Types.WhammDataType.functionType:
+                                break;
+
+                            case Types.WhammDataType.localType:
+                                break;
+
+                            case Types.WhammDataType.tableType:
+                                break;
+
+                            case Types.WhammDataType.elementType:
+                                break;
+
+                            case Types.WhammDataType.opProbeType:
+                                break;
+
+                            case Types.WhammDataType.funcProbeType:
+                                break;
+                        }
+                    }
+                }
+            }
+
+        // We need to follow the section ordering because if we do
+        // then the injected wat line range is fixed and won't need to be updated until the next API call
+        return [[], []];
+    }
+
+    static get_whamm_span(cause: Types.WhammCause): undefined | span{
+        switch(cause.tag){
+            case "whamm":
+                return undefined;
+            default:
+                // _value exists!
+                // @ts-ignore
+                let lc0= cause._value.lc0;
+                // @ts-ignore
+                let lc1= cause._value.lc1;
+
+                return {
+                    lc0: {l: lc0.l, c:lc0.c},
+                    lc1: {l: lc1.l, c:lc1.c}
+                } as span;
+        }
     }
 }
