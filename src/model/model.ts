db5fd73a2d0b632ebcd2a17390d2df9ad188e66c
Supreme Paudel <supremepaudel49@gmail.com>
2025-07-15 09:20:24 -0400

    Update svelte dist code

--- src/model/model.ts
+++ src/model/model.ts
@@ -9,10 +9,12 @@ import { Cell } from "./utils/cell";
 // Class to store API responses [ MVC pattern's model ] 
 export class APIModel{
     static whamm_file_changing: boolean = false;
+    api_response_setup_completed: boolean = false;
 
     // will be null if wizard option chosen
     valid_wat_content!: string ;
     valid_wasm_content!: Uint8Array;
+    whamm_live_injections!: WhammLiveInjections;
 
     injected_wat_content!: string;
     jagged_array: (Cell|null)[][];
@@ -22,9 +24,9 @@ export class APIModel{
     fsm_mappings: FSM | null;
 
     /**
-     *  @todo: Remove every `injected_fsm_mappings` usage in this and model_helper.ts file upon
+     *  @todo: Upon orca funcID fix: Remove every `injected_fsm_mappings` usage in this and model_helper.ts file upon
+     * orca ( or wirm as they now say it) funcID fix
      **/
-    // orca ( or wirm as they now say it) funcID fix
     injected_fsm_mappings: FSM | null;
 
     constructor(webview: WhammWebviewPanel){
@@ -62,6 +64,7 @@ export class APIModel{
                 this.fsm_mappings.run();
                 // create the mappings
                 this.update(true);
+                this.api_response_setup_completed = true;
 
                 return [true, "success"];
             } catch(error){
@@ -77,7 +80,14 @@ export class APIModel{
         let file_path: string | undefined = ExtensionContext.context.workspaceState.get('whamm-file');
         if (file_path && this.webview.fileName){
 
-            let file_contents = await APIModel.loadFileAsString(file_path, ExtensionContext.context);
+            var file_contents: string;
+            let editor = vscode.window.activeTextEditor;
+            if (editor?.document.uri.fsPath === file_path){
+                file_contents = editor.document.getText();
+            } else{
+                file_contents = await APIModel.loadFileAsString(file_path, ExtensionContext.context);
+            }
+
             if (force_update || !ExtensionContext.api.noChange(file_contents)){
                 try{
                     // Call the whamm API to get the response
@@ -95,17 +105,16 @@ export class APIModel{
                     if (this.fsm_mappings != null){
                         
                         /**
-                         *  @todo: Remove once orca fixes the injected fsm mappings and pass in `this.fsm_mappings` instead of `this.injected_fsm_mappings`
+                         *  @todo: Upon orca funcID fix: Remove once orca fixes the injected fsm mappings and pass in `this.fsm_mappings` instead of `this.injected_fsm_mappings`
                          *          for the `ModelHelper.create_whamm_live_injection_instances` static method
                          **/
                         this.injected_fsm_mappings = ModelHelper.update_fsm_funcIDs(this.fsm_mappings, whamm_live_mappings);
-                        let whamm_live_injections = ModelHelper.create_whamm_live_injection_instances(this.injected_fsm_mappings, whamm_live_mappings)
+                        this.whamm_live_injections = ModelHelper.create_whamm_live_injection_instances(this.injected_fsm_mappings, whamm_live_mappings)
 
                         // update the injected wat content
-                        this.injected_wat_content = ModelHelper.inject_wat(this.valid_wat_content, whamm_live_injections.injecting_injections, whamm_live_injections.lines_injected).join('\n');
+                        this.injected_wat_content = ModelHelper.inject_wat(this.valid_wat_content, this.whamm_live_injections.injecting_injections, this.whamm_live_injections.lines_injected).join('\n');
                         // update the jagged array
-                        this.update_jagged_array(whamm_live_injections);
-
+                        this.update_jagged_array(this.whamm_live_injections, file_contents);
                         // success so return true
                         return true;
                     } else{
@@ -122,39 +131,33 @@ export class APIModel{
         return false;
     }
 
-    async update_jagged_array(response: WhammLiveInjections){
-        let whamm_file: string | undefined= ExtensionContext.context.workspaceState.get("whamm-file");
-        if (whamm_file !== undefined){
-            // load the whamm file into a new jagged array
-            let whamm_file_contents = await APIModel.loadFileAsString(whamm_file, ExtensionContext.context);
-            this.jagged_array = ModelHelper.create_jagged_array(whamm_file_contents);
+    async update_jagged_array(response: WhammLiveInjections, file_contents: string){
+        this.jagged_array = ModelHelper.create_jagged_array(file_contents);
 
-            // loop over every injection
-            // and for each injection's span
-            // inject the wamm-live-injection object into that cell of the jagged array
-            for (let injections of [response.injecting_injections, response.other_injections]){
-                for (let whamm_live_injection of injections){
-                    if (whamm_live_injection.whamm_span === null) continue;
+        // loop over every injection
+        // and for each injection's span
+        // inject the wamm-live-injection object into that cell of the jagged array
+        for (let injections of [response.injecting_injections, response.other_injections]){
+            for (let whamm_live_injection of injections){
+                if (whamm_live_injection.whamm_span === null) continue;
 
-                    // Get all the (line,col) pairs for the injection's span
-                    let line_col_values = ModelHelper.get_line_col_values(whamm_live_injection.whamm_span, this.jagged_array);
-                    let span_size = line_col_values.length;
-                    if (span_size <= 0) continue;
+                // Get all the (line,col) pairs for the injection's span
+                let line_col_values = ModelHelper.get_line_col_values(whamm_live_injection.whamm_span, this.jagged_array);
+                let span_size = line_col_values.length;
+                if (span_size <= 0) continue;
 
-                    // loop over each (line,col) pair and add the injection to its cell value
-                    for (let line_col of line_col_values){
-                        let row = line_col[0] -1;
-                        let col = line_col[1] -1;
-                        let cell = this.jagged_array[row][col];
-                        if (cell === null){
-                            this.jagged_array[line_col[0] -1][line_col[1] -1] = new Cell(whamm_live_injection, span_size);
-                        } else {
-                            cell.add(whamm_live_injection, span_size);
-                        }
+                // loop over each (line,col) pair and add the injection to its cell value
+                for (let line_col of line_col_values){
+                    let row = line_col[0] -1;
+                    let col = line_col[1] -1;
+                    let cell = this.jagged_array[row][col];
+                    if (cell === null){
+                        this.jagged_array[line_col[0] -1][line_col[1] -1] = new Cell(whamm_live_injection, span_size);
+                    } else {
+                        cell.add(whamm_live_injection, span_size);
                     }
-            }}
-        } else
-            throw new Error("No whamm file selected! Should not be able to `update` the model");
+                }
+        }}
     }
 
     // file related helper methods
