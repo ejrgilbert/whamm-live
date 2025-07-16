c01e27a248188aafa2562835e768d38b1b40fcc6
Supreme Paudel <supremepaudel49@gmail.com>
2025-07-16 12:51:50 -0400

    API loading button: Add svelte loading animation when the API is being updated

--- svelte/src/lib/WhammApiTracker.svelte
+++ svelte/src/lib/WhammApiTracker.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     function get_file_name(file_path: string){
-        let file_path_array = file_path.split('\\');
+        let file_path_array = file_path.split('/');
         return file_path_array[file_path_array.length -1];
     }
+    import { Jumper } from 'svelte-loading-spinners';
 
     // Reactive variables
-    // Array of [wasm file, api is uptodate or not]
+    // Array of [wasm file, api is out of date or not]
     let api_values: [string, boolean][]= $state([]);
 
     // event listener to update html on change to workspace data
@@ -20,10 +21,44 @@
 
 </script>
 
-{#each api_values as api_value}
-<p>File name: {get_file_name(api_value[0])} Out-of-date: {api_value[1]}</p>
-{/each}
+<div id="footer">
+    <hr style="border-color:grey;">
+    {#each api_values as api_value}
+        <div class="container">
+        {#if api_value[1]}
+            <div class="flex-item">
+            <Jumper size="15" duration="2s" />
+            </div>
+            <div class="flex-item">
+                <p>Updating API for {get_file_name(api_value[0])}</p>
+            </div>
+        {:else} 
+            <div class="flex-item">
+                <p>✅</p>
+            </div>
+            <div class="flex-item">
+                <p>API updated for {get_file_name(api_value[0])}</p>
+            </div>
+        {/if}
+        </div>
+    {/each}
+</div>
 
 <style>
+#footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.container{
+    color: grey;
+    display: flex;
+}
+
+.flex-item{
+    flex-grow: 1;
+    opacity: 70%;
+}
 </style>
 
