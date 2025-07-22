baa461ef1038873c01d9fe508ee1a61936d3bc28
Supreme Paudel <supremepaudel49@gmail.com>
2025-07-22 17:25:37 -0400

    FSM section reorder: Implement the methods associated with each state

--- src/model/fsm_section_order.ts
+++ src/model/fsm_section_order.ts
@@ -3,8 +3,13 @@
  * @remarks Look at the state machine for this with the link above
  * */ 
 
-import { InjectType, sectionNamesInOrder, stringToInjectType } from "./types";
-import { FSMHelper, stack_value } from "./utils/fsm_helper";
+import { sectionNamesInOrder } from "./types";
+import { FSMHelper } from "./utils/fsm_helper";
+
+type stack_value = {
+    section_name: string,
+    start_index: number,
+}
 
 enum State{
     module_state, // qo
@@ -19,6 +24,7 @@ export class FSMSectionReorder{
     wat_string: string;
     private current_state: State;
     private wat_sections: Record<string, string[]>;
+    private section_stack: stack_value[] = [];
 
     static state_to_method_mapping: Record<State, (arg0: FSMSectionReorder) => void > = {
         [State.module_state]: FSMSectionReorder.module_state_method,
@@ -27,6 +33,7 @@ export class FSMSectionReorder{
         // the value function will never be executed
         [State.null_state]: ()=>{},
     }
+    private static section_names: string[]= Object.values(sectionNamesInOrder);
 
     constructor(wat_contents: string){
         this.current_index = 0;
@@ -35,7 +42,7 @@ export class FSMSectionReorder{
 
         this.wat_sections = {};
         // Section ordering is preserved
-        for (let section of Object.values(sectionNamesInOrder)){
+        for (let section of FSMSectionReorder.section_names){
             this.wat_sections[section] = [];
         }
     }
@@ -49,24 +56,89 @@ export class FSMSectionReorder{
 
     // Every state has its own method
 
-    private static module_state_method(){
-        /** 
-         * @todo
-         * */
+    // Start state until components are supported
+    private static module_state_method(instance: FSMSectionReorder){
+        FSMHelper.consume_empty_spaces(instance);
+
+        switch (FSMHelper.get_char(instance)){
+            case '(':
+                {
+                    let start_index = instance.current_index;
+                    let inject_type : string = FSMHelper.get_word(instance);
+                    if (inject_type === "module"){
+                        instance.section_stack.push(
+                            {section_name: inject_type, start_index: start_index});
+                        // transition to `main_state`
+                        instance.current_state = State.main_state;
+                        break;
+                    }
+
+                    // fall through if not "module"
+                }
+            default:
+                throw new Error("FSM parse error: Expected `module` at the start of target wat");
+        }
 
     }
 
-    private static main_state_method(){
-        /** 
-         * @todo
-         * */
+    private static main_state_method(instance: FSMSectionReorder){
+        FSMHelper.consume_empty_spaces(instance);
+        switch (FSMHelper.get_char(instance)){
+            case '(':
+                {
+                    let start_index = instance.current_index;
+                    let section_name : string = FSMHelper.get_word(instance);
 
+                    if (!FSMSectionReorder.section_names.includes(section_name)){
+                        if (FSMHelper.get_char(instance) == '@') {
+                            section_name = "custom";
+                        } else{
+                            throw new Error(`FSM reorder error: Unsupported section name: ${section_name}`)
+                        }
+                    }
+
+                    instance.section_stack.push(
+                                {section_name: section_name, start_index: start_index});
+                    instance.current_state = State.consume_state;
+                }
+                break;
+            case ')':
+                {
+                    // it should **only** be the module section on top of the stack
+                    let value = instance.section_stack.pop();
+                    if (!value || value.section_name !== "module" || instance.section_stack.length !== 0) throw new Error("FSM reorder error: Expected only module on stack."); 
+                    instance.current_index++;
+                    instance.current_state = State.null_state;
+                }
+                break;
+        }
     }
 
-    private static consume_state_method(){
-        /** 
-         * @todo
-         * */
+    private static consume_state_method(instance: FSMSectionReorder){
+        // consume until the closing ')'
+        let expected_section = instance.section_stack[instance.section_stack.length-1];
+        FSMHelper.consume_until_closing_parenthesis(instance);
+        switch (FSMHelper.get_char(instance)){
+            case ')':
+                {
+                    // pop the value off the stack 
+                    let recieved_section = instance.section_stack.pop();
+                    if (expected_section !== recieved_section) throw new Error(`FSM reorder error: Expected end of ${expected_section.section_name}, got end for ${recieved_section?.section_name}`)
+                    if (instance.section_stack.length !== 1) throw new Error(`FSM reorder error: Expeced only module to be on stack, but that isn't the case`)
 
+                    // store this section's span in its respective array
+                    let end_index = instance.current_index+1;
+                    let section_array = instance.wat_sections[expected_section.section_name];
+                    section_array.push(
+                        instance.wat_string.slice(expected_section.start_index,end_index));
+
+                    // Transition back to main state
+                    instance.current_index++;
+                    instance.current_state = State.main_state;
+                }
+                break;
+            default:
+                throw new Error(`FSM reorder error: Expected end of section ${instance.section_stack[instance.section_stack.length-1]}`)
+        }
     }
 }
